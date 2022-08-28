a955a69a2c2440b04ad0024bf07e0c9b2eef7c19
FelipeMasil <felipe.manoel.silva@gmail.com>
2022-08-28 17:33:37 -0300

    add more styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header>
+            <img src="/logo.svg" alt="Podcastr" />
+        </header>
+    )
+}
